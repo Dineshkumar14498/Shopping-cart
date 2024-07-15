1756b0248bd63def060b847e5296aaa28a3bc51f
Dineshkumar14498 <dineshvlr9500@gmail.com>
2024-07-15 20:07:10 +0530

    pending

--- src/Components/Poster.jsx
+++ src/Components/Poster.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+
 const Poster = () => {
     return (
       <div className='Poster-container'>
--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -1,16 +1,18 @@
 import React from "react"
 import Card from "./Card"
 
-// product images 
+import product1 from "../assets/product1.jpg"
 
-const product = [
-  {  title: "iphone 13", price: "Rs.59900" },
+
+
+const pricedetails = [
+  {img: product1, title: "iphone 13", price: "Rs.59900" },
   { title: "iphone 14", price: "Rs.69900" },
- {  title: "iphone 15", price: "Rs.79900" },
-  {  title: "iphone 15 Plus", price: "Rs.99900" },
-  {  title: "iphone 15 pro", price: "Rs.134900" },
-  {  title: "iphone 15 pro Max", price: "Rs.159900" },
-  
+  { title: "iphone 15", price: "Rs.79900" },
+  { title: "iphone 15 Plus", price: "Rs.99900" },
+  { title: "iphone 15 pro", price: "Rs.134900" },
+  { title: "iphone 15 pro Max", price: "Rs.159900" },
+
 ]
 
 //ProductsContainer Component
@@ -18,7 +20,8 @@ const Product = ({ setProductCount }) => {
   return (
     <div className="row product-container g-0 d-flex justify-content-center">
       {
-        product.map(product => (
+        pricedetails.map(product => (
+          
           <Card setProductCount={setProductCount} img={product.img} title={product.title}
             price={product.price} />
         ))
